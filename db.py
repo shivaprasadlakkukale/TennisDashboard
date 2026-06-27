716fa2f45cef530f0d742d7e68264c67056dddf4
Shivaprasadlakkukale <shivaprasad22423@gmail.com>
2026-06-27 20:25:35 +0530

    Fix db.py

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host=st.secrets["reseau.proxy.rlwy.net"],
-        port=int(st.secrets[ "49113"]),
-        user=st.secrets["root"],
-        password=st.secrets["LEyhLpcGoxbCpyvRlbphPiCTgxFHHzDD"],
-        database=st.secrets["railway"]
+        host=st.secrets["MYSQLHOST"],
+        port=int(st.secrets["MYSQLPORT"]),
+        user=st.secrets["MYSQLUSER"],
+        password=st.secrets["MYSQLPASSWORD"],
+        database=st.secrets["MYSQLDATABASE"]
     )
