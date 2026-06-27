79f29e0c8147c034cdbd513dd0822a32cf12a39d
Shivaprasadlakkukale <shivaprasad22423@gmail.com>
2026-06-27 20:23:13 +0530

    Fix Railway database connection

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
+import streamlit as st
 import mysql.connector
 
 def get_connection():
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root123",
-        database="tennis"
+    return mysql.connector.connect(
+        host=st.secrets["reseau.proxy.rlwy.net"],
+        port=int(st.secrets[ "49113"]),
+        user=st.secrets["root"],
+        password=st.secrets["LEyhLpcGoxbCpyvRlbphPiCTgxFHHzDD"],
+        database=st.secrets["railway"]
     )
-    return conn
